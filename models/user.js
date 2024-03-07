a4c8aed3e7781fd46be5d5cc2d986481ca0b9f16
EdwardBecerra2 <146788503+EdwardBecerra2@users.noreply.github.com>
2024-03-07 11:08:59 -0800

    added email validiation regex in user models, and set up thought.js models with schema Thought and Reaction.

--- models/user.js
+++ models/user.js
@@ -4,11 +4,16 @@ const UserSchema = new Schema({
 
     username: {
         type: String,
-        required: true
+        unique: true,
+         required: true,
+         trim: true
+       
     },
     email: {
         type: String,
-        required: true
+        unique: true,
+        required: true,
+        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]  //regex for email validation
     },
     thought: [ {
         type: SchemaType.objectID,
@@ -34,4 +39,5 @@ UserSchema.virtual('friendCount').get(function () {
     return this.friend.length;
 });
 const User = model('User', UserSchema);
+
 module.exports = User;
